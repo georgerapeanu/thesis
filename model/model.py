485df0ad05005c24859cb9814e9caf480df41969
georgerapeanu <alexandrurapeanu@yahoo.com>
2024-02-15 20:25:44 +0200

    quickfix

--- model/model.py
+++ model/model.py
@@ -214,7 +214,7 @@ class ResidualEncoder(nn.Module):
         super(ResidualEncoder, self).__init__()
         self.res_layers = torch.nn.Sequential(
             *[ResidualBlock(in_channels=in_channels, intermediary_channels=intermediary_channels) for _ in range(block_count)],
-            nn.Conv2d(in_channels=in_channels, out_channels=2 * in_channels, kernel_size=1, stride=1, padding=0, bias=False),
+            nn.Conv2d(in_channels=in_channels, out_channels=2 * in_channels, kernel_size=1, stride=1, padding='same', bias=False),
             nn.MaxPool2d(kernel_size=2, stride=2),
             nn.BatchNorm2d(2 * in_channels),
             nn.ReLU(inplace=True)
